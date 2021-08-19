729767edc40c3a41ab83c92f1761a2506637d611
TheBluWiz <88697112+TheBluWiz@users.noreply.github.com>
2021-08-18 22:46:38 -0700

    Create workflowvanishingAct.sh

---
+++ .github/workflowvanishingAct.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+Echo "Would you like to hide, reveal, encrypt or decrypt a volume?"
+read userinput
+
+if [ "$userinput" == "hide" ] ; then
+echo "Which volume would you like to hide?"
+read volume
+sudo setFile -a V /Volumes/"$volume"
+killall Finder
+
+elif [ "$userinput" == "reveal" ] ; then
+echo "Which volume would you like to reveal?"
+read volume
+sudo setFile -a v /Volumes/"$volume"
+
+elif [ "$userinput" == "encrypt" ] ; then
+echo "Which volume would you like to encrypt?"
+read volume
+disk=$(diskutil list)
+disk=$(grep "$volume" <<< "$disk")
+disk=$(printf '%s\n' "${disk:(-7)}")
+diskutil apfs lockVolume "$disk"
+
+## This disk=$ section is pretty fun. It takes user input to discover the disk and volume number.
+
+elif [ "$userinput" == "decrypt" ] ; then
+echo "Which volume would you like to decrypt?"
+read volume
+disk=$(diskutil list)
+disk=$(grep "$volume" <<< "$disk")
+disk=$(printf '%s\n' "${disk:(-7)}")
+diskutil apfs unlockVolume "$disk"
+
+## The disk and volume numbers are parse as disk?s?. The Disk? being a APFS container and that last digit
+## as the volume number.
+
+else
+echo "Unexpected Input"
+echo "Closing Terminal"
+
+fi
