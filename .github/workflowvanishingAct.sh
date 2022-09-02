6090e3d57869330bc34c5dfe1909e9583d248b69
theBluWiz <88697112+TheBluWiz@users.noreply.github.com>
2022-09-02 13:57:37 -0700

    Update workflowvanishingAct.sh

--- .github/workflowvanishingAct.sh
+++ .github/workflowvanishingAct.sh
@@ -13,6 +13,9 @@ echo "Which volume would you like to reveal?"
 read volume
 sudo setFile -a v /Volumes/"$volume"
 
+
+## This disk=$ section is pretty fun. It takes user input to discover the disk and volume number.
+
 elif [ "$userinput" == "encrypt" ] ; then
 echo "Which volume would you like to encrypt?"
 read volume
@@ -20,8 +23,6 @@ disk=$(diskutil list)
 disk=$(grep "$volume" <<< "$disk")
 disk=$(printf '%s\n' "${disk:(-7)}")
 diskutil apfs lockVolume "$disk"
-
-## This disk=$ section is pretty fun. It takes user input to discover the disk and volume number.
 
 elif [ "$userinput" == "decrypt" ] ; then
 echo "Which volume would you like to decrypt?"
@@ -31,7 +32,7 @@ disk=$(grep "$volume" <<< "$disk")
 disk=$(printf '%s\n' "${disk:(-7)}")
 diskutil apfs unlockVolume "$disk"
 
-## The disk and volume numbers are parse as disk?s?. The Disk? being a APFS container and that last digit
+## The disk and volume numbers are parsed as disk?s?. The Disk? being a container/partition and that last digit
 ## as the volume number.
 
 else
